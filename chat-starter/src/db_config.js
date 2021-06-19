94da542c5ad1670b0a4bc27a6c2a58ea2ab9e601
Kristof Matuszka <matuszkak@gmail.com>
2021-06-19 21:31:23 +0200

    chat kész

--- chat-starter/src/db_config.js
+++ chat-starter/src/db_config.js
@@ -1,10 +1,10 @@
 const config = {
-  apiKey: '  AIzaSyABA4KMIaDyDlE0J2sOe6XkKtqBfSArNhc',
-  authDomain: 'chat-app-wsuf-ac474.firebaseapp.com',
-  databaseURL: 'https://chat-app-wsuf-ac474.firebaseio.com',
-  projectId: 'chat-app-wsuf-ac474',
-  storageBucket: 'chat-app-wsuf-ac474.appspot.com',
-  messagingSenderId: '176854093314'
+  apiKey: 'AIzaSyB2M-sa2zH9QiQ5hlmlmq4488jfpoEEz68',
+  authDomain: 'chat-app2-286b8.firebaseapp.com',
+  databaseURL: 'https://chat-app2-286b8.firebaseio.com',
+  projectId: 'chat-app2-286b8',
+  storageBucket: 'chat-app2-286b8.appspot.com',
+  messagingSenderId: '98566996559'
 };
 
 export default config;
